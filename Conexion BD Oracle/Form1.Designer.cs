8f955ab568371b7e01c8ce9b35c1f6e41649b36e
Picart Fernando <63126629+FerniPicart@users.noreply.github.com>
2024-12-03 10:32:30 -0300

    Add files via upload

---
+++ Conexion BD Oracle/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Conexion_BD_Oracle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_estado = new System.Windows.Forms.Label();
+            this.btn_ConectarBD = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_estado
+            // 
+            this.lbl_estado.AutoSize = true;
+            this.lbl_estado.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_estado.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lbl_estado.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_estado.Location = new System.Drawing.Point(30, 30);
+            this.lbl_estado.Name = "lbl_estado";
+            this.lbl_estado.Size = new System.Drawing.Size(194, 34);
+            this.lbl_estado.TabIndex = 0;
+            this.lbl_estado.Text = "Sin Conexión.";
+            // 
+            // btn_ConectarBD
+            // 
+            this.btn_ConectarBD.FlatAppearance.BorderSize = 4;
+            this.btn_ConectarBD.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_ConectarBD.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ConectarBD.Location = new System.Drawing.Point(758, 30);
+            this.btn_ConectarBD.Name = "btn_ConectarBD";
+            this.btn_ConectarBD.Size = new System.Drawing.Size(194, 82);
+            this.btn_ConectarBD.TabIndex = 1;
+            this.btn_ConectarBD.Text = "Conectar a BD";
+            this.btn_ConectarBD.UseVisualStyleBackColor = true;
+            this.btn_ConectarBD.Click += new System.EventHandler(this.btn_ConectarBD_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(10)))), ((int)(((byte)(70)))));
+            this.ClientSize = new System.Drawing.Size(982, 553);
+            this.Controls.Add(this.btn_ConectarBD);
+            this.Controls.Add(this.lbl_estado);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Coral;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Conexion a BD de Netsuite Oracle";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_estado;
+        private System.Windows.Forms.Button btn_ConectarBD;
+    }
+}
+
---
+++ Conexion BD Oracle/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OracleClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Odbc;
+
+namespace Conexion_BD_Oracle
+{
+    public partial class Form1 : Form
+    {
+        // CMD:     odbcad32.exe
+        OdbcConnection db_conn = new OdbcConnection(@"Dsn=nts;
+                                                    HST=5155922.connect.api.netsuite.com;
+                                                    PRT=1708;
+                                                    UID=lgras@cedal.com.ar;
+                                                    password=Net-Lg242024;");
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ConectarBD();
+        }
+
+        private void ConectarBD()
+        {
+            try
+            {
+                // Crear la conexión
+                using (db_conn)
+                {
+                
+                    if (btn_ConectarBD.Text == "Conectar a BD")
+                    {
+                        // Abrir la conexión
+                        db_conn.Open();
+                        MessageBox.Show("Conexión exitosa a Oracle.");
+
+                        if (db_conn.State == ConnectionState.Open)
+                        {
+                            lbl_estado.Text = "Conectado a BD Netsuite.";
+                            btn_ConectarBD.Text = "Desconectar BD";
+                        }
+
+                        // consultas SQL u otras operaciones en la base de datos
+                        // OracleCommand command = new OracleCommand("SELECT * FROM mi_tabla", connection);
+                        // OracleDataReader reader = command.ExecuteReader();
+                        // while (reader.Read())
+                        // {
+                        //     
+                        // }
+                    }
+                    else
+                    {
+                        // Abrir la conexión
+                        db_conn.Close();
+                        MessageBox.Show("Se ha cerrado la conexión a Oracle.");
+
+                        if (db_conn.State == ConnectionState.Closed)
+                        {
+                            lbl_estado.Text = "Sin Conexión.";
+                            btn_ConectarBD.Text = "Conectar a BD";
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al conectar a Oracle: \n-Error: " + ex.Message);
+            }
+        }
+
+        private void btn_ConectarBD_Click(object sender, EventArgs e)
+        {
+            ConectarBD();
+        }
+    }
+}
